711012507715fb380f9f71489a2d99cf97165a5d
Rafael José <rafaelmarqueszeca3@gmail.com>
2025-05-21 19:47:35 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// game 1-monopoly game 2- ticket to ride game 3- takeneko 
+
+function alterarStatus(id) {
+let = gameClicado = document.getElementById(`game-${id}`)
+let = imagem = gameClicado.querySelector('.dashboard__item__img');
+let = botao = gameClicado.querySelector('.dashboard__item__button');
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) { 
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return')
+        botao.textContent = 'Alugar'
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.textContent = 'Devolver';
+        botao.classList.add('dashboard__item__button--return');
+    }
+    
+   
+}
