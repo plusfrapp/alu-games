4c3a52c12a22afcc7cf5fcfbc4e45715d0a9e577
Rafael José <rafaelmarqueszeca3@gmail.com>
2025-05-21 20:02:13 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -7,8 +7,8 @@ let = botao = gameClicado.querySelector('.dashboard__item__button');
 
     if (imagem.classList.contains('dashboard__item__img--rented')) { 
         imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return')
-        botao.textContent = 'Alugar'
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.textContent = 'Devolver';
